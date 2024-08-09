5cdcf7bbe186ad22a93d01a633382faff13317d2
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-09 19:33:09 +0000

    chore: Add loader animation and update GitHub API integration

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -1,7 +1,13 @@
 function fetchGitHubInformation(event) {
     let username = $('#gh-username').val();
     if (!username) {
-        $('#gh-user-data').html('<h2>Please enter a GitHub username</h2>');
+        $('#gh-user-data-nousername').html('<h2 class="text-center">Please enter a GitHub username</h2>');
         return;
     }
+
+    $('#loader-container').html(
+        `<div class="loader" alt="loading...">
+            
+        </div>`
+    );
 }
