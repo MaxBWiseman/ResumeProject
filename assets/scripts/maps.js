27e7c182306e5c4fe0a688c0c25c71ceb1a8f9c3
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-09 16:35:44 +0000

    style: Update image URL for beachMarker in initMap function

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -4,7 +4,7 @@ function initMap() {
       center: { lat: 50.75000000, lng: -3.75000000 },
     });
     const image =
-      "";
+      "https://1drv.ms/i/c/17800d23338d1269/IQOmfmaO_eGLTYy90u269wztAVKmfn9y34X5ZZ3Sg8PLXPo?width=30";
     const beachMarker = new google.maps.Marker({
       position: { lat: 50.75000000, lng: -3.75000000 },
       map,
