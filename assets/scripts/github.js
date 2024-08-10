a077b0627a8f9b91ebf87d0d3e3dfd349ac9c675
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-10 12:35:09 +0000

    feat: Add user information HTML template and fetch GitHub API data

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -1,3 +1,20 @@
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
 function fetchGitHubInformation(event) {
     let username = $('#gh-username').val();
     if (!username) {
@@ -9,5 +26,21 @@ function fetchGitHubInformation(event) {
         `<div class="loader" alt="loading...">
             
         </div>`
+    );
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            let userData = response;
+            $('#gh-user-data').html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $('#gh-user-data').html(`<h2 class="text-center">No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $('#gh-user-data').html(`<h2 class="text-center">Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        }
     );
 }
