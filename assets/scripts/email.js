92a97b6bbff0c8a8faa74bcef4784245702fee50
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-12 13:59:23 +0000

    style: Update email form submission in contact.html

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,8 +1,8 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("default_value", "resume_request_1dnfsx8", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.comment.value
     })
     .then(
         function(response) {
